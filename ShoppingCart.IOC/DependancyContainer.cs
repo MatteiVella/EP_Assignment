b6ab3b9eb72b7ea7aea17f9d3593ecd99b438369
matte <matteivella9@gmail.com>
2020-12-14 19:00:08 +0100

    Created the DependancyContainer for Dependancy Injection

---
+++ ShoppingCart.IOC/DependancyContainer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver.Core.Configuration;
+using ShoppingCart.Data.Context;
+using ShoppingCart.Data.Repositories;
+using ShoppingCart.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace ShoppingCart.IOC
+{
+    public class DependancyContainer
+    {
+        public static void RegisterServices(IServiceCollection services, String connectionString)
+        {
+
+            services.AddDbContext<ShoppingCartDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddScoped<IProductsRepository, ProductsRepository>();
+        }
+    }
+}
